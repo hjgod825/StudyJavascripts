d644e7b13e43c34c303dbd883ebf254f295b60d5
x3249444@gmail.com <x3249444@gmail.com>
2025-01-08 16:14:07 +0900

    변수와 상수

---
+++ learn-javascript/lectures/3_javascript/variable.js
@@ -0,0 +1,13 @@
+// //scope = 블럭 동일한 의미인듯?
+// {
+//   let a = '변수';
+//   console.log(a);
+// }
+// //a는 스코프 안에서 썻기때문에 밖에서 못씀
+// //전역 지역변수 너무기본
+// // console.log('a', a);
+
+// function sum(a, b) {
+//   let result = a + b;
+//   return result;
+// }
